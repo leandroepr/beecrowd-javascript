1e7c25f986b585a0dffe1892abb0efea4c389fe1
Leandro Reis <leandro.e.reis@gmail.com>
2024-02-18 16:14:34 -0300

    refactor: remove unnecessary files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const main = require('./src/main');
+const Solution = require('./src/core/solution');
 
-main();
+const problem1000 = '1.beginner/1000.hello-world/solutions/main.js';
+const solution = new Solution(problem1000);
+console.log(solution.execute('')); // Hello World!
--- src/main.test.js
+++
@@ -1,6 +0,0 @@
-const sum = require('./sum');
-
-test('Deve testar o sum', () => {
-  const result = sum(2, 3);
-  expect(result).toBe(5);
-});
